5fcdd39d64d6c3bf75156d2e6257897f3d6063e9
Ken Hauter <kenneth.hauter@gmail.com>
2024-11-18 15:55:49 +0900

    Add card component to portfolio project
    added in a card component that took props from the
    ProjectsSection.js file

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,38 @@
-import { Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
+import { Box, Heading, HStack, Image, Text, VStack } from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
 
-const Card = ({ title, description, imageSrc }) => {
+const Card = ({title, description, imageSrc }) => {
   // Implement the UI for the Card component according to the instructions.
   // You should be able to implement the component with the elements imported above.
   // Feel free to import other UI components from Chakra UI if you wish to.
-  return null;
+  return (
+    <div>
+      <HStack spacing={3}>
+        <Box 
+          rounded="xl" 
+          background="white"
+          textColor="black"
+          paddingBottom={5}
+        >
+          <VStack spacing={3}>
+              <Image src={imageSrc} alt={title} rounded="xl"></Image>
+        <Box paddingX={4}>
+          <Heading padddingY={3} size="md">
+            {title}
+          </Heading>
+        <Text>{description}</Text>
+        <HStack spacing={2}>
+          <Text fontWeight="bold">See More</Text>
+          <FontAwesomeIcon icon={faArrowRight} size="1x"/>
+        </HStack>
+        </Box>
+          </VStack>
+        </Box>
+      </HStack>
+    </div>
+  );
 };
 
 export default Card;
